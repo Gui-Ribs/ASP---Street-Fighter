09cb8e8cb82a76783dbc0a08d2a8f545bc4de0c0
Phoenix-141 <gui.ribs2000@gmail.com>
2022-12-02 22:50:14 -0300

    finish

---
+++ ds_mvc_asp/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ds_mvc_asp.Models
+{
+    public class Usuario
+    {
+        [Display (Name = "Nome de lutador:")]
+        [Required (ErrorMessage = "O nome é obrigatório")]
+
+        public string NomeUsuario { get; set;  }
+
+
+        [Display (Name = "Idade:")]
+        [Range (15, 70, ErrorMessage = "Obrigatório inserir idade entre 15 e 70 anos")]
+
+        public string Idade { get; set; }
+
+        [Display (Name = "E-mail:")]
+        [RegularExpression (@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Email inválido")]
+
+        public string Email { get; set; }
+
+
+        [Display (Name = "Senha:")]
+        [Required (ErrorMessage = "Senha obrigatória")]
+
+        public string Senha { get; set; }
+
+        [Display(Name = "Confirmar senha:")]
+        [System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage = "A senha não corresponde")]
+        public string Confsenha { get; set; }
+
+    }
+}
